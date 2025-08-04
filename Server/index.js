e8c0b636d21e4e55618a3014ddeedbc29f272799
Nawang17 <nawangofficial1010@gmail.com>
2025-08-04 13:28:12 -0400

    Refactor API to manage links: implement getLinks, addLink, and deleteLink functions

--- Server/index.js
+++ Server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const helmet = require("helmet");
-const db = require("./queries");
+const { getLinks, addLink, deleteLink } = require("./queries");
 const app = express();
 const port = 3000;
 
@@ -14,11 +14,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get("/", (req, res) =>
   res.json({ info: "Node.js, Express, and Postgres API" })
 );
-app.get("/users", db.getUsers);
-app.get("/users/:id", db.getUserById);
-app.post("/users", db.createUser);
-app.put("/users/:id", db.updateUser);
-app.delete("/users/:id", db.deleteUser);
+app.get("/links", getLinks);
+app.post("/links", addLink);
+app.delete("/links/:id", deleteLink);
 
 app.listen(port, () => {
   console.log(`App running on port ${port}.`);
